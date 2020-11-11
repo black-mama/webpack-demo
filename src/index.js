68aa32e4587308c9c546e0211c78536ed512d3e0
Yu Ma <1091550187@qq.com>
2020-11-12 01:12:29 +0800

    管理资源

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import _ from "lodash"
+import _ from "lodash";
+import './style.css';
+import icon from './cat.jpg';
 function component() {
     const element = document.createElement('div');
 
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack','ok?'], ' ');
+    element.classList.add('hello');
 
+    const myIcon = new Image();
+    myIcon.src = icon;
+    element.appendChild(myIcon);
     return element;
 }
 
