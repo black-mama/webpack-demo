0e45361f914133078e70c6a19a4ffb6576da2852
Yu Ma <1091550187@qq.com>
2020-11-12 10:38:17 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import _ from "lodash";
-import './style.css';
-import icon from './cat.jpg';
+import printMe from "./print";
 function component() {
     const element = document.createElement('div');
 
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack','ok?'], ' ');
-    element.classList.add('hello');
 
-    const myIcon = new Image();
-    myIcon.src = icon;
-    element.appendChild(myIcon);
+    const btn = document.createElement('button');
+    btn.innerHTML = "点击此处";
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
 }
 
